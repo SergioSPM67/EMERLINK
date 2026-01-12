9677fce92971287189ec0896a19adf1cac64d509
SergioSPM67 <xlmgato6@gmail.com>
2026-01-12 21:30:41 +0100

    Update AlertSender.java
    Actualización manual del archivo AlertSender.java ya que no se me había actualizado cuando había hecho el comit en vs code y me acabo de dar cuanta al repasar la actividad...

--- src/main/java/com/emergencias/alert/AlertSender.java
+++ src/main/java/com/emergencias/alert/AlertSender.java
@@ -2,6 +2,7 @@ package com.emergencias.alert;
 
 import com.emergencias.model.EmergencyEvent;
 import java.io.FileWriter;
+import java.io.IOException;
 
 public class AlertSender {
 
@@ -12,10 +13,19 @@ public class AlertSender {
         System.out.println("Alerta enviada a 112:");
         System.out.println(event);
 
+        mostrarAlertaFormateada(event);
+
         try (FileWriter writer = new FileWriter("alertas.txt", true)) {
             writer.write(event.toString() + "\n");
-        } catch (Exception e) {
-            System.err.println("Error al guardar la alerta: " + e.getMessage());
+        } catch (IOException e) {
+        System.err.println("Error de entrada/salida al guardar la alerta");
         }
     }
+
+private void mostrarAlertaFormateada(EmergencyEvent event) {
+    System.out.println("===== ALERTA DE EMERGENCIA =====");
+    System.out.println(event.toString());
+    System.out.println("================================");
+}
+
 }
